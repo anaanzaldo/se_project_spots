eda5d727b3d4cc05abc19d0c5393576a84ae1bc5
anaanzaldo <aridianaanzaldo@yahoo.com>
2025-04-14 12:06:59 -0500

    js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,24 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#Profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#Profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editeditProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,6 +33,13 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  console.log("submitting");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
 const initialcards = [
   {
